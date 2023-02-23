8b6368b4dccd4cdecd9b88a374d7f06694ed7bd0
Melona-Png <madisont.woodward@gmail.com>
2023-02-23 18:11:24 -0500

    added user model

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+// Schema to create user model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: [validateEmail, 'Please enter a valid email to proceed.']
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+}
+);
+
+// friendCount Virtual
+userSchema.virtual('friendCount').get(function () {
+return this.friends.length
+})
+
+const User = model('User', userSchema)
+
+module.exports = User
