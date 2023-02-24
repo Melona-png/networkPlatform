9a541aca350ebeb0b7adf28e8b83fceba819645c
Melona-Png <madisont.woodward@gmail.com>
2023-02-23 19:38:57 -0500

    stuck on bug coming back to this later

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,22 +1,21 @@
 const router = require("express").Router();
-
 const {
-  getAllUsers,
+  getAllUser,
   getUserById,
   createUser,
   updateUser,
   deleteUser,
   addFriend,
   removeFriend,
-} = require("/config/connection");
+} = require("../../controllers/userControllers");
 
-// Set up GET all and POST at /api/users
-router.route("/").get(getAllUsers).post(createUser);
+// /api/users
+router.route("/").get(getAllUser).post(createUser);
 
-// Set up GET one, PUT, and DELETE at /api/users/:id
+// /api/users/:id
 router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 
-// Add and delete a friend
-router.route("/:id/friends/:friendsId").post(addFriend).delete(removeFriend);
+// /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 
 module.exports = router;
