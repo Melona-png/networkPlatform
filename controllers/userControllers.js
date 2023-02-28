a0ab295208337c55fd07996c698febb702150117
Melona-Png <madisont.woodward@gmail.com>
2023-02-28 09:33:06 -0500

    fixed friends in user controllers need to mess with routes

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -52,4 +52,44 @@ module.exports = {
       )
       .catch((err) => res.status(500).json(err));
   },
+     // delete user
+     deleteUser(req, res) {
+      User.findOneAndDelete({ _id: req.params.id }).then((user) => !user ? res.status(404).json({ message: 'No user with that ID' }) : Thought.deleteMany({
+          _id: {
+              $in: user.thoughts
+          }
+      })).then(() => res.json({ message: 'User and associated apps deleted!' })).catch((err) => res.status(500).json(err));
+  },
+  // getUserById,
+  getUserById(req, res) {
+      User.findOne({ _id: req.params.id }).then((user) => !user ? res.status(404).json({ message: 'No user with that ID' }) : res.json(user)).catch((err) => res.status(500).json(err));
+  },
+  // addFriend
+  addFriend(req, res) {
+      console.log('You are adding a friend');
+      console.log(req.body);
+      User.findOneAndUpdate({
+          _id: req.params.id
+      }, {
+          $addToSet: {
+              friends: req.params.friendsId
+          }
+      }, {
+          runValidators: true,
+          new: true
+      }).then((user) => !user ? res.status(404).json({ message: 'No friend found with that ID :(' }) : res.json(user)).catch((err) => res.status(500).json(err));
+  },
+  // removeFriend
+  removeFriend(req, res) {
+      User.findOneAndUpdate({
+          _id: req.params.id
+      }, {
+          $pull: {
+              friends: req.params.friendsId
+          }
+      }, {
+          runValidators: true,
+          new: true
+      }).then((user) => !user ? res.status(404).json({ message: 'No friend found with that ID :(' }) : res.json(user)).catch((err) => res.status(500).json(err));
+  }
 };
